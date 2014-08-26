79e39b07a013775f29dc04a1f4b4bc3194a1511e
Hasegawa Chisato <chase0213@gmail.com>
2014-08-26 18:42:04 +0900

    modify version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="geocodecs",
-    version='0.1.1',
+    version='0.1.2',
     description="Python wrapper for Google Maps API",
     author="HASEGAWA Chisato",
     author_email="chase0213+dev@gmail.com",
